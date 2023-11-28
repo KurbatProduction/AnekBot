129108ca0b43d4301a6b6c8c73a84167a2befda7
Sergey Kurbatov <131869069+KurbatProduction@users.noreply.github.com>
2023-11-28 14:07:28 +0300

    change parcer's url

--- parcer.py
+++ parcer.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-URL_TEMPLATE = "https://www.anekdot.ru/search/?rubrika=j&query="
+URL_TEMPLATE_START = "https://www.anekdot.ru/search/?query="
+URL_TEMPLATE_END = "&ch%5Bj%5D=on&mode=any&xcnt=100&maxlen=&order=0"
 
 
 def parce(key_words):
     key_words = '+'.join(key_words.split())
-    r = requests.get(URL_TEMPLATE + key_words)
+    r = requests.get(URL_TEMPLATE_START + key_words + URL_TEMPLATE_END)
     soup = bs(r.text, "html.parser")
     anek_list = soup.find_all('div', class_='text')
     anek_list_text = []
